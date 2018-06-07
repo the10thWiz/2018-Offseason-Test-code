dddd1c773a1eddf1ac75e16e23b8a376668a8012
Cvisaya1 <Cvisaya1@10.103.1.2>
2018-06-06 19:53:05 -0500

    add Robot, map and OI from Christian

---
+++ src/org/usfirst/frc/team1732/robot/chris/Robot.java
@@ -0,0 +1,142 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team1732.robot.chris;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team1732.robot.commands.auto.DriveTest;
+import org.usfirst.frc.team1732.robot.commands.auto.drive.DriveToAngleWithRadius;
+import org.usfirst.frc.team1732.robot.commands.auto.drive.DriveWithEncoders;
+import org.usfirst.frc.team1732.robot.commands.auto.drive.PointTurn;
+import org.usfirst.frc.team1732.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team1732.robot.subsystems.Manipulator;
+import org.usfirst.frc.team1732.robot.subsystems.navx.NavX;
+
+import com.kauailabs.navx.frc.AHRS;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+	public static DriveTrain driveTrain;
+	public static Manipulator manipulator;
+	public static OI m_oi;
+	public static AHRS ahrs;
+	public static NavX navx;
+
+	Command m_autonomousCommand;
+	SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		try {
+			initializeSubsystems();
+			m_oi = new OI();
+			ahrs = new AHRS(SPI.Port.kMXP);
+			navx = new NavX(ahrs);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() {
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit() {
+		Robot.ahrs.zeroYaw();
+		//m_autonomousCommand = new DriveWithEncoders(120); //(FUNCTIONAL)
+		m_autonomousCommand = new DriveToAngleWithRadius(90, 26, false); //(BROKEN)
+		//m_autonomousCommand = new PointTurn(-90); //(FUNCTIONAL)
+		//m_autonomousCommand = new DriveTest(); //(Basically FUNCTIONAL)
+		
+		// m_autonomousCommand = m_chooser.getSelected();
+		
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.start();
+		}
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		driveTrain.resetEncoders();
+		Robot.ahrs.zeroYaw();
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.cancel();
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+	
+	public void initializeSubsystems() {
+		driveTrain = new DriveTrain();
+		manipulator = new Manipulator();
+	}
+}
---
+++ src/org/usfirst/frc/team1732/robot/chris/RobotMap.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team1732.robot.chris;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	// Joysticks
+	public static final int JOYSTICK_Y_AXIS = 1;
+	public static final int JOYSTICK_Z_AXIS = 2;
+
+	public static final int LEFT_JOYSTICK_USB = 0;
+	public static final int RIGHT_JOYSTICK_USB = 1;
+
+	// Drivetrain
+	public static final int LEFT_MASTER_MOTOR_DEVICE_NUMBER = 0;
+	public static final int LEFT_1_MOTOR_DEVICE_NUMBER = 1;
+	public static final int LEFT_2_MOTOR_DEVICE_NUMBER = 2;
+
+	public static final int RIGHT_MASTER_MOTOR_DEVICE_NUMBER = 15;
+	public static final int RIGHT_1_MOTOR_DEVICE_NUMBER = 14;
+	public static final int RIGHT_2_MOTOR_DEVICE_NUMBER = 13;
+	
+	// Manipulator
+	public static final int LEFT_MANIP_MOTOR_NUMBER = 10;
+	public static final int RIGHT_MANIP_MOTOR_NUMBER = 11;
+}
